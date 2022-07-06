b7439de8ca2f529ad75628cc1a2b0858b11c82a1
Blackharddd <djalexmurcer@gmail.com>
2022-07-06 23:52:24 +0300

    -added pagination to FAQ;

--- faq.php
+++ faq.php
@@ -9,6 +9,14 @@
 
 get_header();
 
+$current_url = ccpt_get_current_url();
+
+$posts_per_page = carbon_get_the_post_meta( 'items_per_page' );
+$items = carbon_get_the_post_meta( 'items' );
+
+$current_page = isset( $_GET['offset'] ) ? $_GET['offset'] : 1;
+$max_num_pages = intval( ceil( count( $items ) / $posts_per_page ) );
+
 ?>
 
 <section id="faq">
@@ -17,24 +25,61 @@ get_header();
             <h1 class="title page-header__title"><?php the_title(); ?></h1>
         </div>
 
-        <?php if( $accordeon_items = carbon_get_the_post_meta( 'items' ) ) : ?>
+        <?php if( $items ) : ?>
             <div class="accordeon accordeon--faq">
-                <?php foreach( $accordeon_items as $item ) : ?>
-                    <div class="accordeon-item accordeon-item--faq">
-                        <div class="accordeon-item__header">
-                            <div class="accordeon-item__title"><?=$item['title']; ?></div>
+                <?php foreach( $items as $index => $item ) : ?>
+                    <?php if( ( $max_num_pages > 1 && $index >= $current_page * $posts_per_page - $posts_per_page && $index < $current_page * $posts_per_page ) || $max_num_pages === 1 ) : ?>
+                        <div class="accordeon-item accordeon-item--faq">
+                            <div class="accordeon-item__header">
+                                <div class="accordeon-item__title"><?=$item['title']; ?></div>
 
-                            <div class="accordeon-item__icon"></div>
-                        </div>
+                                <div class="accordeon-item__icon"></div>
+                            </div>
 
-                        <div class="accordeon-item__body">
-                            <div class="accordeon-item__content">
-                                <?=wpautop( $item['description'] ); ?>
+                            <div class="accordeon-item__body">
+                                <div class="accordeon-item__content">
+                                    <?=wpautop( $item['description'] ); ?>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    <?php endif; ?>
                 <?php endforeach; ?>
             </div>
+
+            <?php if( count( $items ) > $posts_per_page ) : ?>
+                <div class="pagination">
+                        <?php if( $current_page > 1 ) : ?>
+                            <a href="<?=ccpt_add_pagination_query_params( $current_page - 1, $current_url, 'offset' ); ?>" class="arrow-button arrow-button--left pagination__prev">
+                                <div class="arrow-button__icon">
+                                    <?=ccpt_get_icon( 'arrow-left/circle' ); ?>
+                                    <?=ccpt_get_icon( 'arrow-left/arrow' ); ?>
+                                </div>
+                            </a>
+                        <?php endif; ?>
+
+                        <div class="pagination__pages">
+                            <?php for( $index = 1; $index <= $max_num_pages; $index++ ) : ?>
+                                <?php
+                                    
+                                $is_active = ( !isset( $_GET['offset'] ) && $index === 1 ) || ( isset( $_GET['offset'] ) && $_GET['offset'] == $index );
+                                $link = ccpt_add_pagination_query_params( $index, $current_url, 'offset' );
+                                
+                                ?>
+
+                                <a <?=!$is_active ? "href='{$link}'" : ""; ?> class="pagination__link <?=$is_active ? 'pagination__link--active' : ''; ?>"><?=$index; ?></a>
+                            <?php endfor; ?>
+                        </div>
+
+                        <?php if( $current_page < $max_num_pages ) : ?>
+                            <a href="<?=ccpt_add_pagination_query_params( $current_page + 1, $current_url, 'offset' ); ?>" class="arrow-button arrow-button--right pagination__next">
+                                <div class="arrow-button__icon">
+                                    <?=ccpt_get_icon( 'arrow-right/arrow' ); ?>
+                                    <?=ccpt_get_icon( 'arrow-right/circle' ); ?>
+                                </div>
+                            </a>
+                        <?php endif; ?>
+                    </div>
+            <?php endif; ?>
         <?php endif; ?>
     </div>
 </section>
--- inc/carbon-fields/faq.php
+++ inc/carbon-fields/faq.php
@@ -6,6 +6,9 @@ use Carbon_Fields\Field;
 Container::make( 'post_meta', __( 'Налаштування сторінки', 'ce-crypto' ) )
     ->where( 'post_template', '=', 'faq.php' )
     ->add_fields( array(
+        Field::make( 'text', 'items_per_page', __( 'Кількість питань на сторінці', 'ce-crypto' ) )
+            ->set_attribute( 'type', 'number' )
+            ->set_default_value( 10 ),
         Field::make( 'complex', 'items', __( 'Питання', 'ce-crypto' ) )
             ->add_fields( array(
                 Field::make( 'text', 'title', __( 'Заголовок', 'ce-crypto' ) ),
