d3818768d4629ce797cd8c462b51fb1eebcb5e04
Blackharddd <djalexmurcer@gmail.com>
2022-07-07 13:58:36 +0300

    -added stick article in archive option;

--- inc/carbon-fields/article.php
+++ inc/carbon-fields/article.php
@@ -7,6 +7,8 @@ use Carbon_Fields\Field;
 Container::make( 'post_meta', __( 'Налаштування статті', 'ce-crypto' ) )
     ->where( 'post_type', '=', 'article' )
     ->add_fields( array(
+        Field::make( 'checkbox', 'sticky_archive', __( 'Закріпити в архіві', 'ce-crypto' ) )
+            ->set_option_value( '1' ),
         Field::make( 'text', 'reading_time', __( 'Час читання (хв.)', 'ce-crypto' ) )
             ->set_attribute( 'type', 'number' )
             ->set_default_value( 5 )
